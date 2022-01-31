a014030383ae8b8992707cbc73ad379ddab50820
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-01-31 16:28:43 -0300

    Cria header básico para a aplicação

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import profile from '../../images/profileIcon.svg';
 import search from '../../images/searchIcon.svg';
+import PropTypes from 'prop-types';
 
-function Header() {
+// 360 x 640
+function Header({ history }) {
+
+  const [toggleSearch, setToggleSearch] = useState(false);
+
   return (
-  <div>
-    <img
-      src={profile}
-      alt="Profile Icon"
-      data-testid="profile-top-btn"
-    />
-    <h1
-      data-testid="page-title"
-    >
-      Profile
-    </h1>
-    <img
-      src={search}
-      alt="Search Icon"
-      data-testid="search-top-btn"
-    />
-
-  </div>
+    <div>
+      <button
+        type="button"
+        data-testid="profile-top-btn"
+        onClick={ () => history.push('/profile') }
+      >
+        <img
+          src={ profile }
+          alt="Profile Icon"
+        />
+      </button>
+      <h1
+        data-testid="page-title"
+      >
+        Profile
+      </h1>
+      <button
+        type="button"
+        data-testid="search-top-btn"
+        onClick={ () => setToggleSearch(!toggleSearch) }
+      >
+        <img
+          src={ search }
+          alt="Search Icon"
+        />
+      </button>
+      {toggleSearch && (
+        <input
+          type="text"
+          data-testid="search-input"
+        />
+      )}
+    </div>
   );
 }
+
+Header.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Header;
