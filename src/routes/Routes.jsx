737d78a758f7479fa8c4aff31c06c251af110205
mabiane <mabiane.polniak@gmail.com>
2022-02-02 11:08:37 -0300

    conflito anterior resolvido

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
-import { Route, Switch } from 'react-router-dom';
-=======
 import { Switch, Route } from 'react-router-dom';
 import Profile from '../components/Profile';
 import Header from '../components/Header/Header';
->>>>>>> 9a6a012c0616155fb7dd69349d9c6d5762e5fc9d
 
 export default function Routes() {
   return (
