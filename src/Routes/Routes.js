b597ca64480ff7d7cf47ebf2fd636d367a9cd1a8
mabiane <mabiane.polniak@gmail.com>
2022-02-02 10:28:38 -0300

    redireciona ao clicar

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
+import Recipes from '../pages/Recipes';
 import Explore from '../pages/Explore';
 import ExploreFoods from '../pages/ExploreFoods';
 import ExploreDrinks from '../pages/ExploreDrinks';
@@ -15,8 +16,8 @@ function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route exact path="/foods" />
-      <Route exact path="/drinks" />
+      <Route exact path="/foods" component={ Recipes } />
+      <Route exact path="/drinks" component={ Recipes } />
       <Route exact path="/foods/{id-da-receita}" />
       <Route exact path="/drinks/{id-da-receita}" />
       <Route exact path="/foods/{id-da-receita}/in-progress" />
--- src/components/MenuInferior/MenuInferior.js
+++ src/components/MenuInferior/MenuInferior.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Drinkicon from '../../images/drinkIcon.svg';
 import ExploreIncon from '../../images/exploreIcon.svg';
 import FoodIcon from '../../images/mealIcon.svg';
 
 export default function MenuInferior() {
+  const history = useHistory();
+
   return (
     <footer
       className="fixed-bottom d-flex justify-content-between"
       data-testid="footer"
     >
-      <button data-testid="food-bottom-btn" type="button" source={ FoodIcon }>
+      <button
+        data-testid="food-bottom-btn"
+        type="button"
+        src={ FoodIcon }
+        onClick={ () => history.push('/foods') }
+      >
         <img src={ FoodIcon } alt="ilustração com taça" />
       </button>
-      <button data-testid="drinks-bottom-btn" type="button" source={ Drinkicon }>
+      <button
+        data-testid="drinks-bottom-btn"
+        type="button"
+        src={ Drinkicon }
+        onClick={ () => history.push('/drinks') }
+      >
         <img src={ Drinkicon } alt="ilustração com taça" />
       </button>
-      <button data-testid="explore-bottom-btn" type="button" source={ ExploreIncon }>
+      <button
+        data-testid="explore-bottom-btn"
+        type="button"
+        src={ ExploreIncon }
+        onClick={ () => history.push('/explore') }
+      >
         <img src={ ExploreIncon } alt="ilustração com taça" />
       </button>
     </footer>
