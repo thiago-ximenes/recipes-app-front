7c773d2a4d3d7cf99f6c49de4bc82402ed1b3828
Meiryland Melo <meirylandmelo@gmail.com>
2022-02-02 13:58:49 -0300

    adiciona Header a tela principal

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import CardRecipe from '../components/Card/CardRecipe';
+import Header from '../components/Header/Header';
 
 function Recipes() {
   return (
     <div>
+      <Header />
       <CardRecipe />
     </div>
   );
