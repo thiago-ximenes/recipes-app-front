120a117f2ffe216e92012281736f96d43912035b
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-02-02 20:26:36 -0300

    passa no requisito 17

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,46 +1,75 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Card, Container, Row } from 'react-bootstrap';
 import Header from '../components/Header/Header';
 import MenuInferior from '../components/MenuInferior/MenuInferior';
 import MyContext from '../Context/MyHeaderSearchContext/MyContent';
 
 function Recipes() {
-  const { loading, data, domainName } = useContext(MyContext);
+  const history = useHistory();
+
+  const { loading, data } = useContext(MyContext);
 
   const [recipeType, setRecipeType] = useState('meals');
 
   useEffect(() => {
+    const domainName = history.location.pathname.split('/')[1];
     const routName = domainName === 'foods' ? 'meals' : 'drinks';
+    console.log(routName);
     setRecipeType(routName);
-  });
+  }, []);
 
   const recipeTypeCapitalized = recipeType.charAt(0)
     .toUpperCase() + recipeType.slice(1, recipeType.length - 1);
+
+  const ELEVEN = 11;
+
+  function renderData() {
+    let result = [];
+    if (data[recipeType].length > 1) {
+      result = data[recipeType].map((recipe, index) => {
+        if (index <= ELEVEN) {
+          return (
+            <Row
+              className="justify-content-md-center"
+              data-testid={ `${index}-recipe-card` }
+            >
+              <Card
+                style={ { width: '18rem' } }
+                key={ index }
+              >
+                <Card.Title
+                  data-testid={ `${index.toString()}-card-name` }
+                >
+                  {recipe[`str${recipeTypeCapitalized}`]}
+                </Card.Title>
+                <img
+                  alt={ `${recipe[`str${recipeTypeCapitalized}`]}` }
+                  src={ recipe[`str${recipeTypeCapitalized}Thumb`] }
+                  data-testid={ `${index.toString()}-card-img` }
+                />
+              </Card>
+            </Row>
+          );
+        }
+        return null;
+      });
+    }
+    return result;
+  }
 
   return (
     !loading && (
       <div>
         <Header />
-        { !data[recipeType]
-          ? (
-            <h2>{domainName}</h2>
-          ) : data[recipeType].map((recipe, index) => (
-            <Card
-              style={ { width: '18rem' } }
-              data-testid={ `${index}-recipe-card` }
-              key={ index }
-            >
-              <Card.Title
-                data-testid={ `${index}-recipe-card-name` }
-              >
-                {recipe[`str${recipeTypeCapitalized}`]}
-              </Card.Title>
-              <Card.Img
-                variant="top"
-                src={ recipe[`str${recipeTypeCapitalized}Thumb`] }
-              />
-            </Card>
-          ))}
+        <Container
+          fluid="sm"
+        >
+          { !data[recipeType]
+            ? (
+              <h2>Recipes</h2>
+            ) : renderData()}
+        </Container>
         <MenuInferior />
       </div>
     )
