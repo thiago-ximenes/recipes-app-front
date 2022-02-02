22fcbdcf78fef1b4d2bb17f8ba13095841fea91b
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-02-02 18:01:59 -0300

    resolve problema de lint no component recipes

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,18 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Button, Card } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import MenuInferior from '../components/MenuInferior/MenuInferior';
 import MyContext from '../Context/MyHeaderSearchContext/MyContent';
 
 function Recipes() {
-  const history = useHistory();
   const { loading, data } = useContext(MyContext);
-
-  useEffect(() => {
-    if (data.meals) history.push('/foods');
-    if (data.drinks) history.push('/drinks');
-  }, [data]);
 
   return (
     !loading && (
@@ -25,7 +18,6 @@ function Recipes() {
               <Card.Title>Card Title</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
-                the card's content.
               </Card.Text>
               <Button
                 variant="primary"
