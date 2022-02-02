4ddd34cfb24fd47aee28045058170f0d5644a689
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-02-02 18:52:04 -0300

    trabalhando no requisito 17

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,32 +1,46 @@
-import React, { useContext } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import React, { useContext, useState, useEffect } from 'react';
+import { Card } from 'react-bootstrap';
 import Header from '../components/Header/Header';
 import MenuInferior from '../components/MenuInferior/MenuInferior';
 import MyContext from '../Context/MyHeaderSearchContext/MyContent';
 
 function Recipes() {
-  const { loading, data } = useContext(MyContext);
+  const { loading, data, domainName } = useContext(MyContext);
+
+  const [recipeType, setRecipeType] = useState('meals');
+
+  useEffect(() => {
+    const routName = domainName === 'foods' ? 'meals' : 'drinks';
+    setRecipeType(routName);
+  });
+
+  const recipeTypeCapitalized = recipeType.charAt(0)
+    .toUpperCase() + recipeType.slice(1, recipeType.length - 1);
 
   return (
     !loading && (
       <div>
         <Header />
-        { data.length > 0 && (
-          <Card style={ { width: '18rem' } }>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-              </Card.Text>
-              <Button
-                variant="primary"
+        { !data[recipeType]
+          ? (
+            <h2>{domainName}</h2>
+          ) : data[recipeType].map((recipe, index) => (
+            <Card
+              style={ { width: '18rem' } }
+              data-testid={ `${index}-recipe-card` }
+              key={ index }
+            >
+              <Card.Title
+                data-testid={ `${index}-recipe-card-name` }
               >
-                Go somewhere
-
-              </Button>
-            </Card.Body>
-          </Card>) }
+                {recipe[`str${recipeTypeCapitalized}`]}
+              </Card.Title>
+              <Card.Img
+                variant="top"
+                src={ recipe[`str${recipeTypeCapitalized}Thumb`] }
+              />
+            </Card>
+          ))}
         <MenuInferior />
       </div>
     )
