72e1dd063d435650261fccb0d3a81a388f15520a
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-02-10 19:30:17 -0300

    resolve 21

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import DoneRecipesMain from '../components/DoneRecipesMain/DoneRecipesMain';
 import Header from '../components/Header/Header';
-import MenuInferior from '../components/MenuInferior/MenuInferior';
 
 function DoneRecipes() {
   return (
     <div>
       <Header title="Done Recipes" />
       <DoneRecipesMain />
-      <MenuInferior />
     </div>
   );
 }
