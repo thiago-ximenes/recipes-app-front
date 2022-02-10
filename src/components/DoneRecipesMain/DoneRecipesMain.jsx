f95815a64fee5152c9a8fec73b2edb73bc45b583
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-02-10 17:40:49 -0300

    passa no requisito 57 e 58

--- src/components/DoneRecipesMain/DoneRecipesMain.jsx
+++ src/components/DoneRecipesMain/DoneRecipesMain.jsx
@@ -4,6 +4,7 @@ import ShareButton from '../RecipesDetailsPage/ShareButton';
 
 function DoneRecipesMain() {
   const [doneRecipes, setDoneRecipes] = useState([]);
+  const [filter, setFilter] = useState('all');
 
   useEffect(() => {
     if (localStorage.getItem('doneRecipes') !== null) {
@@ -11,85 +12,94 @@ function DoneRecipesMain() {
     }
   }, []);
 
-  console.log(doneRecipes);
-
   return (
     <div>
       <button
         type="button"
         data-testid="filter-by-all-btn"
+        onClick={ () => setFilter('all') }
       >
         All
       </button>
       <button
         type="button"
         data-testid="filter-by-food-btn"
+        onClick={ () => setFilter('food') }
       >
         Food
       </button>
       <button
         type="button"
         data-testid="filter-by-drink-btn"
+        onClick={ () => setFilter('drink') }
       >
         Drinks
       </button>
-      {doneRecipes.length && doneRecipes.map((recipe, index) => (
-        <Card
-          key={ recipe.name }
-        >
-          <Card.Img
-            variant="top"
-            src={ recipe.image }
-            data-testid={ `${index}-horizontal-image` }
-          />
-          <Card.Body>
-            <Card.Text
-              data-testid={ `${index}-horizontal-name` }
-            >
-              { recipe.name }
-            </Card.Text>
-            <Card.Title
-              data-testid={ `${index}-horizontal-top-text` }
-            >
-              { recipe.name }
-            </Card.Title>
-            { recipe.type === 'food' ? (
+      {doneRecipes.length && doneRecipes.filter((recipe) => {
+        if (filter === 'food') {
+          return recipe.type === 'food';
+        } if (filter === 'drink') {
+          return recipe.type === 'drink';
+        }
+        return true;
+      })
+        .map((recipe, index) => (
+          <Card
+            key={ recipe.name }
+          >
+            <Card.Img
+              variant="top"
+              src={ recipe.image }
+              data-testid={ `${index}-horizontal-image` }
+            />
+            <Card.Body>
               <Card.Text
+                data-testid={ `${index}-horizontal-name` }
+              >
+                { recipe.name }
+              </Card.Text>
+              <Card.Title
                 data-testid={ `${index}-horizontal-top-text` }
               >
-                { `${recipe.nationality} - ${recipe.category}`}
-              </Card.Text>
-            )
-              : (
+                { recipe.name }
+              </Card.Title>
+              { recipe.type === 'food' ? (
                 <Card.Text
                   data-testid={ `${index}-horizontal-top-text` }
                 >
-                  {recipe.alcoholicOrNot}
+                  { `${recipe.nationality} - ${recipe.category}`}
                 </Card.Text>
-              )}
-            <span
-              data-testid={ `${index}-horizontal-done-date` }
-            >
-              {recipe.doneDate}
-            </span>
-            <ShareButton
-              testId={ `${index}-horizontal-share-btn` }
-              hrefLink={ `http://localhost:3000/${recipe.type}s/${recipe.id}` }
-            />
-            <span>
-              {recipe.tags.map((tag) => (
-                <span
-                  key={ tag }
-                  data-testid={ `${index}-${tag}-horizontal-tag` }
-                >
-                  {tag}
-                </span>
-              ))}
-            </span>
-          </Card.Body>
-        </Card>
+              )
+                : (
+                  <Card.Text
+                    data-testid={ `${index}-horizontal-top-text` }
+                  >
+                    {recipe.alcoholicOrNot}
+                  </Card.Text>
+                )}
+              <span
+                data-testid={ `${index}-horizontal-done-date` }
+              >
+                {recipe.doneDate}
+              </span>
+              <ShareButton
+                testId={ `${index}-horizontal-share-btn` }
+                hrefLink={ `http://localhost:3000/${recipe.type}s/${recipe.id}` }
+              />
+              <span>
+                {recipe.tags.map((tag) => (
+                  <span
+                    key={ tag }
+                    data-testid={ `${index}-${tag}-horizontal-tag` }
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </span>
+            </Card.Body>
+          </Card>
 
-      ))}
+        ))}
     </div>
   );
 }
